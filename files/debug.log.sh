cef15e643481859aa4eac1b5285a4104eb2c89c7
M3TIOR <m3tior@darklight-team.org>
2016-05-18 03:32:09 -0700

    added debug controller

---
+++ files/debug.log.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#M3TIOR 2016
+
+#Debug redirection unit
+
+if ! [ -e ~/.i3/settings/debug.redirect ]; then
+	mkdir -p /var/log/i3;
+	echo "/var/log/i3/debug.log" > ~/.i3/settings/debug.redirect;
+fi
+
+while read line; do
+	echo "$*" >> $line;
+done < ~/.i3/settings/debug.redirect
